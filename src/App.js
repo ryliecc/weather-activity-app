5672b6426a50266c3b0ed4a1404617c794c43335
Rylie Castell <rylie.castell@gmail.com>
2023-06-02 10:31:54 +0200

    set up basic app structure

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Header>
+    <WeatherSymbol />
+    <Temperature />
+    <Text />
+  </Header>
+  <Main>
+    <ActivityList>
+      <Activity>
+        <Text />
+        <DeleteButton />
+    </Activity>
+    </ActivityList>
+    <Form>
+      <Heading />
+      <Input />
+      <Input />
+      <Button />
+    </Form>
+    </Main>
+
   );
 }
 
